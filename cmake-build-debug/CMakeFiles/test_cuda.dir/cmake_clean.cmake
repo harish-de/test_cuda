8b246acc096b7e69531322d232101fe9f6db658c
harish-de <harish.harihara@st.ovgu.de>
2020-06-30 09:51:38 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/test_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cuda.dir/cmake_device_link.o"
+  "CMakeFiles/test_cuda.dir/main.cu.o"
+  "test_cuda"
+  "test_cuda.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/test_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
